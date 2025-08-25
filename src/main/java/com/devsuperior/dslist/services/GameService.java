df089a1c973654bdd2e8eb9c2d11926f2d49eeb0
João Penteado <jbbpenteado@hotmail.com>
2025-08-25 16:36:27 -0300

    Database seed update + Get Game by Id

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
@@ -12,8 +13,14 @@ import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
 public class GameService {
+	
 	@Autowired
 	private GameRepository gameRepository;
+	
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	} 
 	
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
